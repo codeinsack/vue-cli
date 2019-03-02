b8b75eadc38b29b93aee75d663fb03fa8e0c936f
codeinsack <codeinsack@gmail.com>
2019-03-02 13:02:30 +0300

    folder structure

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <appHeader />
+    <hr>
+    <div class="row">
+      <Servers />
+      <appServerDetails />
+    </div>
+    <hr>
+    <appFooter />
+  </div>
+</template>
+
+<script>
+import Header from './components/Shared/Header.vue';
+import Footer from './components/Shared/Footer.vue';
+import Servers from './components/Server/Servers.vue';
+import ServerDetails from './components/Server/ServerDetails.vue';
+
+export default {
+  components: {
+    appHeader: Header,
+    Servers,
+    appServerDetails: ServerDetails,
+    appFooter: Footer,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
